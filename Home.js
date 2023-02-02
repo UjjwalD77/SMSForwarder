795f8e061dc7bd67d80c5a9ede0ae9021d99ea76
Ujjwal <devreujjval77@gmail.com>
2023-02-02 05:49:11 +0530

    add contacts to components

--- Home.js
+++ Home.js
@@ -1,28 +1,37 @@
-import { StyleSheet, Text, View, Switch, ScrollView, Button, PermissionsAndroid } from 'react-native'
+import { StyleSheet, Text, View, Switch, ScrollView, Button, PermissionsAndroid, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { selectContactPhone } from 'react-native-select-contact';
 
 const Home = () => {
+  const contactsTemplate= {}
+  console.log(JSON.stringify(contactsTemplate))
+  
   const [isFirstLaunch, setIsFirstLauch] = useState();  
+  const [savedList, setSavedList] = useState(contactsTemplate);
 
   const initFirstLaunch = async () => {
     try {
       await AsyncStorage.setItem('firstLaunch', "false");
       await AsyncStorage.setItem('appEnabled', "false");
-      await AsyncStorage.setItem('contactsToSend','none' );
-      console.log('first')
+      await AsyncStorage.setItem('contactsToSend',JSON.stringify(contactsTemplate));
     } catch (e) {
       console.log(e)
     }
   }
   const initLaunch = async () => {
     try {
+      // await AsyncStorage.setItem('contactsToSend',JSON.stringify(contactsTemplate));
       const value = await AsyncStorage.getItem('appEnabled');
       console.log(value)
       console.log(typeof (value));
       if (value == "true") {
         setMSIE(true);
+      }
+      const tempList = await AsyncStorage.getItem('contactsToSend');
+      console.log("list "+tempList)
+      if(tempList !== JSON.stringify(contactsTemplate)){
+        setSavedList(JSON.parse(tempList));
       }
     }
     catch (e) {
@@ -32,6 +41,7 @@ const Home = () => {
   }
   const checkFirstLaunch = async () => {
     try {
+      // await AsyncStorage.clear();
       const value = await AsyncStorage.getItem('firstLaunch');
       if (value === null) {
         setIsFirstLauch(true);
@@ -53,6 +63,9 @@ const Home = () => {
     initLaunch();
   }, [])
 
+  useEffect(()=>{
+    
+  },[savedList])
   async function addContact() {
     // on android we need to explicitly request for contacts permission and make sure it's granted
     // before calling API methods
@@ -72,21 +85,36 @@ const Home = () => {
     }
     let { contact, selectedPhone } = selection;
     console.log(`Selected ${selectedPhone.type} phone number ${selectedPhone.number} from ${contact.name}`);
+    let curContacts = await AsyncStorage.getItem("contactsToSend");
+    curContacts = JSON.parse(curContacts);
+    curContacts[selectedPhone.number] = contact.name;
+    await AsyncStorage.setItem("contactsToSend", JSON.stringify(curContacts));
+    console.log('bRO' + JSON.stringify(curContacts))
+
   }    
   
   const getContactsList = async() =>{
-
+    const savedList = await AsyncStorage.getItem('constactsToSend')
   }
   const ContactsList = () =>{
-    // const savedList = await AsyncStorage.getItem('contactsToSend');
-    const savedList = "none";
-    if(savedList === "none"){
+    console.log(typeof(savedList))
+    if(savedList === JSON.stringify(contactsTemplate)){
+      console.log('same?')
       return(
         <Text style={{fontSize: 23, color: 'black', alignSelf: 'center', padding: 20}}>No Contacts Selected!</Text>
       )
     }
+    else{
+      console.log('not same')
+    }
+    
   }
-
+  const Item = ({name, number}) => (
+    <View style={styles.item}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={{fontSize:18, color: 'grey'}} >{number}</Text>
+    </View>
+  );
   const [mainSwitchIsEnabled, setMSIE] = useState(false);
   const toggleMS = async () => {
     const curstate = mainSwitchIsEnabled ? "false" : "true";
@@ -106,10 +134,11 @@ const Home = () => {
           <Text style={{ fontSize: 18, color: 'black', alignSelf: 'center' }}>Send List</Text>
           <Button title='Add Contact' onPress={addContact} ></Button>
         </View>
-        <ScrollView style={styles.listView}>
          <ContactsList/>
-
-        </ScrollView>
+        <FlatList style={styles.listView}
+          data={Object.keys(savedList)}
+          renderItem={({item})=><Item name={savedList[item]} number={item}/>}
+        />
       </View>
     </View>
   )
@@ -134,10 +163,26 @@ const styles = StyleSheet.create({
   sendListHeader: {
     backgroundColor: "#e1f4fa",
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    borderColor: 'red',
+    borderLeftWidth: 2,
+    padding: 4,
+    marginBottom: 20,
   },
   listView:{
+    // flex:1,
     maxHeight: 400,
     backgroundColor: 'lightgrey',
-  }
+    borderRadius: 20,
+  },
+  item: {
+    // backgroundColor: '#f9c2ff',
+    padding: 5,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  name: {
+    fontSize: 20,
+    color: 'black',
+  },
 })
